4b01506257ccafab4eeb7828bb48759b9aa9db69
rinse <rinse@neko2.net>
2023-02-18 23:45:39 +0900

    feat: Add an ArrowRight icon to playing video on a playlist

--- src/components/Playlist.tsx
+++ src/components/Playlist.tsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import {Avatar, List, ListItemAvatar, ListItemButton, ListItemText} from "@mui/material";
+import React from "react";
+import {Avatar, Box, List, ListItemAvatar, ListItemButton, ListItemText} from "@mui/material";
+import {ArrowRight as ArrowRightIcon} from "@mui/icons-material";
 
 export interface PlayListProps {
   videoIds: string[]
   selected?: string
 }
 
+function EmptyIcon() {
+  return <Box sx={{width: 24}}/>;
+}
+
 export default function Playlist(props: PlayListProps) {
   const {videoIds, selected} = props
   return (
     <List sx={{width: "100%", backgroundColor: "background.default"}} component='nav'>
-      {videoIds.map(videoId =>
-        <ListItemButton component='a' key={videoId}
-                        href={`/watch?v=${encodeURI(videoId)}`} selected={videoId === selected}>
-          <ListItemAvatar>
-            <Avatar src={`videos/${videoId}/thumbnail.png`}/>
-          </ListItemAvatar>
-          <ListItemText primary={videoId /* videoTitle */}/>
-        </ListItemButton>
-      )}
+      {videoIds.map(videoId => {
+        const isSelected = videoId === selected;
+        const videoTitle = videoId;
+        return (
+          <ListItemButton component='a' key={videoId}
+                          href={`/watch?v=${encodeURI(videoId)}`} selected={isSelected}>
+            {isSelected ? <ArrowRightIcon/> : <EmptyIcon/>}
+            <ListItemAvatar>
+              <Avatar src={`videos/${videoId}/thumbnail.png`}/>
+            </ListItemAvatar>
+            <ListItemText primary={videoTitle}/>
+          </ListItemButton>
+        )
+      })}
     </List>
   );
 }
