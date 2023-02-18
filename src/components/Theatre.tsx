f2bc8eec0088dcbcf470ccca6844684fdd13a436
rinse <rinse@neko2.net>
2023-02-18 23:05:44 +0900

    feat: Improve Theatre

--- src/components/Theatre.tsx
+++ src/components/Theatre.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import {Typography} from "@mui/material";
+import {Box, Paper, Typography} from "@mui/material";
 import ReactPlayer from 'react-player'
 import PlaylistContainer from './containers/PlaylistContainer'
 
 export interface TheatreProps {
-    video: string
+  video: string
 }
 
 export default function Theatre(props: TheatreProps) {
-    const {video} = props
-    return (
-        <>
-            <ReactPlayer url={`/videos/${video}/index.m3u8`}
-                className='react-player' controls
-                width='100%' height='100%' />
-            <Typography sx={{fontWeight: 400, fontSize: "18px", lineHeight: "2.4rem"}} >{video}</Typography>
-            <PlaylistContainer />
-        </>
-    )
+  const {video} = props;
+  return (
+    <>
+      <ReactPlayer url={`/videos/${video}/index.m3u8`}
+                   className='react-player' controls
+                   width='100%' height='100%'/>
+      <Box sx={{margin: 1}}>
+        <Typography sx={{fontWeight: "bold", fontSize: 18}}>{video}</Typography>
+      </Box>
+      <Paper variant="outlined">
+        <PlaylistContainer/>
+      </Paper>
+    </>
+  )
 }
